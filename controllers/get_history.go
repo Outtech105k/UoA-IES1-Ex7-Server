d6cce6966d3640ce74fd1d19b884cedc744339ba
Outtech105k <techno510tk@gmail.com>
2025-07-02 12:29:51 +0900

    Add History Get, Web UI
    - 履歴取得エンドポイント GET `/history`の実装
    - apiエンドポイントを`/api/*`に変更
    - tailwindcssを活用したフロントエンドをGeminiで実装

--- controllers/get_history.go
+++ controllers/get_history.go
@@ -3,12 +3,20 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/Outtech105k/UoA-IES1-Ex7-Server/repos"
+	"github.com/Outtech105k/UoA-IES1-Ex7-Server/utils"
 	"github.com/gin-gonic/gin"
 )
 
-func GetDetectHistory() gin.HandlerFunc {
-	// TODO: Implement it.
-	return func(ctx *gin.Context) {
-		ctx.String(http.StatusServiceUnavailable, "Now featuring")
+func GetDetectHistory(appCtx utils.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var histories []repos.Histories
+		err := appCtx.DB.Select(&histories, "SELECT * FROM histories ORDER BY detected DESC")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, histories)
 	}
 }
--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/Outtech105k/UoA-IES1-Ex7-Server/controllers"
 	"github.com/Outtech105k/UoA-IES1-Ex7-Server/utils"
 	"github.com/gin-gonic/gin"
@@ -9,8 +11,16 @@ import (
 func SetupRouter(appCtx utils.AppContext) *gin.Engine {
 	r := gin.Default()
 
-	r.POST("/faceauth", controllers.PostFaceAuthHandler(appCtx))
-	r.GET("/history", controllers.GetDetectHistory())
+	r.LoadHTMLGlob("public/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	api := r.Group("/api")
+	{
+		api.POST("/faceauth", controllers.PostFaceAuthHandler(appCtx))
+		api.GET("/history", controllers.GetDetectHistory(appCtx))
+	}
 
 	return r
 }
